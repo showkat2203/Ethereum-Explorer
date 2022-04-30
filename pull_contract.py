f243a068873a99e617f112db890407681fe47f36
showkat2203@diu.edu.bd <showkat2203@diu.edu.bd>
2022-04-30 18:03:40 -0500

    made changes for pulling contract

--- pull_contract.py
+++ pull_contract.py
@@ -7,17 +7,34 @@ from web3 import Web3
 from web3.eth import Eth
 import pprint
 
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/b586327d0bc040b6a56e574a1e3ae332"))
+# w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/b586327d0bc040b6a56e574a1e3ae332"))
 
 import requests
 apipath = 'https://api.etherscan.io/api?'
 apikey = "YK9SX9V14RUKKDHYVBTDRFTYKYEKT1ACZU"
 
+
+# GETH/PARITY Proxy API
+def getBlockNumber():
+    payload = {'module': 'proxy', 'action':'eth_BlockNumber', 'apikey':apikey}
+    return requests.get(apipath, params=payload).json()['result']
+
+def getBlockByNumber(number):
+    payload = {'module':'proxy', 'action':'eth_getBlockByNumber', 'tag': hex(number), 'boolean':'true', 'apikey':apikey}
+    return requests.get(apipath, params=payload).json()['result']
+
 def getSourceCode(address):
 	payload = {'module':'contract', 'action':'getsourcecode', 'address':address, 'apikey':apikey}
 	response = requests.get(apipath, params=payload)
 
-	if response.json()["status"] == 1:
+	# print(response)
+	# print(response.__dict__)
+	# print(response.json())
+	# print("----------------", response.json()["status"])
+
+
+	if response.json()["status"] == "1":
+		# print("******************************************")
 		response = response.json()['result']
 	else:
 		print("getSourceCode Failed ")
@@ -29,10 +46,14 @@ def getContractTxStatus(txhash):
 	payload = {'module':'proxy', 'action':'eth_getTransactionByHash', 'txhash':txhash, 'apikey':apikey}
 	response = requests.get(apipath, params=payload)
 
-	# if len(response.json()["result"]) == 1:
-	response = response.json()['result']
-	# else:
-	# 	print("getContractTxStatus Failed ")
+
+	print(response)
+	print(response.__dict__)
+
+	if len(response.json()["status"]) == "1":
+		response = response.json()['result']
+	else:
+		print("getContractTxStatus Failed ")
 
 	return response
 
@@ -46,8 +67,24 @@ with open('existing_source.txt') as f:
     	contract_list.append(line)
     	contract_cnt += 1
 	
-cur_block = w3.eth.getBlock('latest')
-prev_block = cur_block.number - 1
+# cur_block = w3.eth.getBlock('latest')
+cur_block = getBlockNumber()
+
+cur_block = int(cur_block, 16)
+
+cur_block = getBlockByNumber(cur_block)
+
+prev_block = cur_block["number"]
+print(prev_block)
+prev_block = int(prev_block, 16)
+print(prev_block)
+
+print("Start Block ", prev_block)
+
+# prev_block = cur_block.json()['result']["number"] - 1
+
+# print("Second ", cur_block['transactions'])
+
 
 found = 0
 
@@ -55,43 +92,43 @@ import os
 if not os.path.exists('solidity_source'):
     os.makedirs('solidity_source')
 
-while contract_cnt < 100:
-	for tx in cur_block.transactions:
-		tx_ad = str(tx.hex())
-		retobj = getContractTxStatus(tx_ad)
+while contract_cnt < 200:
+	for tx in cur_block['transactions']:
+		tx_ad = tx['to']
+		if( tx_ad in contract_list ):
+			continue
+		contract = getSourceCode(tx_ad)
 
-		if not (retobj.get('to') is None):
-			if( retobj['to'] in contract_list):
-				continue
+		if( contract == "NotFound"):
+			continue
 
-			contract = getSourceCode(retobj['to'])
-			if( contract == "NotFound"):
-				continue
-
-			if not (contract[0].get('SourceCode') is None):
-				# print("Tx: ", tx_ad, "To ", retobj['to'])
-				my_code = contract[0]['SourceCode']
-				# print(type(my_code))
-				if(len(my_code) > 10):
-					contract_list.append(retobj['to'])
-					# found = 1
-					contract_cnt += 1
-					print("Contract Count: ", contract_cnt)
-					with open(os.path.join('/home/sonnet/Desktop/solidity_source', retobj['to'] + ".sol"), "w") as text_file:
-						lines = my_code.splitlines()
-						for line in lines:
-							text_file.write(line)
+		if not (contract[0].get('SourceCode') is None):
+			# print("Tx: ", tx_ad, "To ", retobj['to'])
+			my_code = contract[0]['SourceCode']
+			# print(type(my_code))
+			if(len(my_code) > 200):
+				contract_list.append(tx_ad)
+				# found = 1
+				contract_cnt += 1
+				print("Contract Count: ", contract_cnt)
+				with open(os.path.join('/home/sonnet/Desktop/custom_etherscan/solidity_source', tx_ad + ".sol"), "w") as text_file:
+					lines = my_code.splitlines()
+					for line in lines:
+						text_file.write(line)
 						# break		
-	if( found == 1):
+		if(contract_cnt > 200):
 		break
 
-	if(contract_cnt > 200):
-		break
+	# if( found == 1):
+	# 	break
 
-	cur_block = w3.eth.getBlock(prev_block)
+	# cur_block = w3.eth.getBlock(prev_block)
+	cur_block = getBlockByNumber(prev_block)
 	prev_block -= 1
 
 
-with open('existing_source.txt', "w") as text_file:
+with open('/home/sonnet/Desktop/custom_etherscan/existing_source.txt', "w") as text_file:
 	for line in contract_list:
-		text_file.write(line)
+		text_file.write(line + "\n")
+
+print("End Block ", prev_block)
